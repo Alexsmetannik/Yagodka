78f3f471f9d88c8514ea53b71db7725b8c8268c4
Alexandr Galkin <alexzagigalkin@yandex.ru>
2025-03-24 20:56:03 +0300

    fix

--- src/main/java/org/yagodka/DemoApplication.java
+++ src/main/java/org/yagodka/DemoApplication.java
@@ -2,8 +2,10 @@ package org.yagodka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "org.yagodka")
 public class DemoApplication {
 
     public static void main(String[] args) {
